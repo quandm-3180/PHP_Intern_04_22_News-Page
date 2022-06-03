a360c53dd24271b33bd2714b1319b7b376821a51
Quan Troy 19 <doquan285lg@gmail.com>
2022-06-03 09:46:16 +0700

    Setup layout client

---
+++ resources/views/client/layout/style.blade.php
@@ -0,0 +1,27 @@
+<!-- google fonts -->
+<link href="{{ asset('bower_components/css/index') }}" rel="stylesheet">
+<!-- Bootstrap -->
+<link rel="stylesheet" href="{{ asset('bower_components/templete-news365/css/bootstrap.min.css') }}">
+<!-- Scrollbar css -->
+<link rel="stylesheet" type="text/css"
+    href="{{ asset('bower_components/templete-news365/css/jquery.mCustomScrollbar.css') }}" />
+<!-- Owl Carousel css -->
+<link rel="stylesheet" type="text/css"
+    href="{{ asset('bower_components/templete-news365/owl-carousel/owl.carousel.css') }}" />
+<link rel="stylesheet" type="text/css"
+    href=" {{ asset('bower_components/templete-news365/owl-carousel/owl.theme.css') }}" />
+<link rel="stylesheet" type="text/css"
+    href="{{ asset('bower_components/templete-news365/owl-carousel/owl.transitions.css') }}" />
+<!-- youtube css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('bower_components/templete-news365/css/RYPP.css') }}" />
+<!-- jquery-ui css -->
+<link rel="stylesheet" href="{{ asset('bower_components/templete-news365/css/jquery-ui.css') }}">
+<!-- animate -->
+<link rel="stylesheet" href="{{ asset('bower_components/templete-news365/css/animate.min.css') }}">
+<!-- fonts css -->
+<link rel="stylesheet" href="{{ asset('bower_components/templete-news365/font-awesome/css/font-awesome.min.css') }}">
+<link rel="stylesheet" type="text/css"
+    href="{{ asset('bower_components/templete-news365/css/Pe-icon-7-stroke.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('bower_components/templete-news365/css/flaticon.css') }}" />
+<!-- custom css -->
+<link rel="stylesheet" href="{{ asset('bower_components/templete-news365/css/style.css') }}">
